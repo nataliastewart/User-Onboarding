9e3e3b1fa9e50059c2c5a2cdd7065058a22ab9c9
nataliastewart <nataliastewart@users.noreply.github.com>
2020-03-25 18:58:09 -0400

    Form done - initial state for inputs ok

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,5 +1,44 @@
 import React from "react";
+import * as yup from "yup";
 
 export default function Form() {
-  return <div>Hello from Form</div>;
+  //managing state for my inputs
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    terms: ""
+  });
+
+  //setting state for errors
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+    terms: ""
+  });
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">
+          Name:
+          <input type="text" name="name" id="name" />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input type="text" name="email" id="email" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input type="password" name="password" id="password" />
+        </label>
+        <label htmlFor="terms" className="terms">
+          Terms & Conditions
+          <input type="checkbox" name="terms" checked={true} />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
