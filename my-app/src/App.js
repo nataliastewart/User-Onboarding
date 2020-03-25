d1eb108250e27d656cc848d5f233fc84a149623e
nataliastewart <nataliastewart@users.noreply.github.com>
2020-03-25 17:49:30 -0400

    Starting project - Form.js created

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+      Hello from App
+      <Form />
+    </div>
+  );
+}
+
+export default App;
